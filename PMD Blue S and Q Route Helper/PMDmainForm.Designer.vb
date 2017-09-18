6b7028c8cafb244a0bb837a52bb46d7fbd61343a
tcessna <tcessna@PUDGEPRO8>
2017-09-18 12:31:14 -0500

    Add project files.

---
+++ PMD Blue S and Q Route Helper/PMDmainForm.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PMDmainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PMDmainForm))
+        Me.lblStaticDungeon = New System.Windows.Forms.Label()
+        Me.lblDungeon = New System.Windows.Forms.Label()
+        Me.lblStaticFloor = New System.Windows.Forms.Label()
+        Me.lblFloor = New System.Windows.Forms.Label()
+        Me.btnPreviousFloor = New System.Windows.Forms.Button()
+        Me.btnNextFloor = New System.Windows.Forms.Button()
+        Me.lblStaticNotes = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cboxDungeon = New System.Windows.Forms.ComboBox()
+        Me.lblManual = New System.Windows.Forms.Label()
+        Me.cboxFloor = New System.Windows.Forms.ComboBox()
+        Me.btnPreviousDungeon = New System.Windows.Forms.Button()
+        Me.btnNextDungeon = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cboxhiddenDungeonSelected = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.picBoxDungeon = New System.Windows.Forms.PictureBox()
+        CType(Me.picBoxDungeon, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblStaticDungeon
+        '
+        Me.lblStaticDungeon.AutoSize = True
+        Me.lblStaticDungeon.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStaticDungeon.Location = New System.Drawing.Point(12, 451)
+        Me.lblStaticDungeon.Name = "lblStaticDungeon"
+        Me.lblStaticDungeon.Size = New System.Drawing.Size(102, 24)
+        Me.lblStaticDungeon.TabIndex = 1
+        Me.lblStaticDungeon.Text = "Dungeon:"
+        '
+        'lblDungeon
+        '
+        Me.lblDungeon.AutoSize = True
+        Me.lblDungeon.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDungeon.Location = New System.Drawing.Point(120, 451)
+        Me.lblDungeon.Name = "lblDungeon"
+        Me.lblDungeon.Size = New System.Drawing.Size(121, 24)
+        Me.lblDungeon.TabIndex = 2
+        Me.lblDungeon.Text = "Tiny Woods"
+        '
+        'lblStaticFloor
+        '
+        Me.lblStaticFloor.AutoSize = True
+        Me.lblStaticFloor.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStaticFloor.Location = New System.Drawing.Point(373, 451)
+        Me.lblStaticFloor.Name = "lblStaticFloor"
+        Me.lblStaticFloor.Size = New System.Drawing.Size(65, 24)
+        Me.lblStaticFloor.TabIndex = 3
+        Me.lblStaticFloor.Text = "Floor:"
+        '
+        'lblFloor
+        '
+        Me.lblFloor.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFloor.Location = New System.Drawing.Point(444, 456)
+        Me.lblFloor.Name = "lblFloor"
+        Me.lblFloor.Size = New System.Drawing.Size(128, 55)
+        Me.lblFloor.TabIndex = 4
+        Me.lblFloor.Text = "1F"
+        Me.lblFloor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnPreviousFloor
+        '
+        Me.btnPreviousFloor.Location = New System.Drawing.Point(444, 511)
+        Me.btnPreviousFloor.Name = "btnPreviousFloor"
+        Me.btnPreviousFloor.Size = New System.Drawing.Size(61, 33)
+        Me.btnPreviousFloor.TabIndex = 5
+        Me.btnPreviousFloor.Text = "Previous"
+        Me.btnPreviousFloor.UseVisualStyleBackColor = True
+        '
+        'btnNextFloor
+        '
+        Me.btnNextFloor.Location = New System.Drawing.Point(511, 511)
+        Me.btnNextFloor.Name = "btnNextFloor"
+        Me.btnNextFloor.Size = New System.Drawing.Size(61, 33)
+        Me.btnNextFloor.TabIndex = 6
+        Me.btnNextFloor.Text = "Next"
+        Me.btnNextFloor.UseVisualStyleBackColor = True
+        '
+        'lblStaticNotes
+        '
+        Me.lblStaticNotes.AutoSize = True
+        Me.lblStaticNotes.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStaticNotes.Location = New System.Drawing.Point(272, 511)
+        Me.lblStaticNotes.Name = "lblStaticNotes"
+        Me.lblStaticNotes.Size = New System.Drawing.Size(48, 15)
+        Me.lblStaticNotes.TabIndex = 7
+        Me.lblStaticNotes.Text = "Notes:"
+        Me.lblStaticNotes.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(272, 528)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(124, 39)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Something stupid you should have memorized"
+        Me.Label1.Visible = False
+        '
+        'cboxDungeon
+        '
+        Me.cboxDungeon.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboxDungeon.FormattingEnabled = True
+        Me.cboxDungeon.Location = New System.Drawing.Point(124, 516)
+        Me.cboxDungeon.Name = "cboxDungeon"
+        Me.cboxDungeon.Size = New System.Drawing.Size(120, 21)
+        Me.cboxDungeon.TabIndex = 9
+        '
+        'lblManual
+        '
+        Me.lblManual.AutoSize = True
+        Me.lblManual.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblManual.Location = New System.Drawing.Point(12, 513)
+        Me.lblManual.Name = "lblManual"
+        Me.lblManual.Size = New System.Drawing.Size(84, 24)
+        Me.lblManual.TabIndex = 10
+        Me.lblManual.Text = "Manual:"
+        '
+        'cboxFloor
+        '
+        Me.cboxFloor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboxFloor.FormattingEnabled = True
+        Me.cboxFloor.Location = New System.Drawing.Point(195, 546)
+        Me.cboxFloor.Name = "cboxFloor"
+        Me.cboxFloor.Size = New System.Drawing.Size(49, 21)
+        Me.cboxFloor.TabIndex = 11
+        '
+        'btnPreviousDungeon
+        '
+        Me.btnPreviousDungeon.Location = New System.Drawing.Point(124, 477)
+        Me.btnPreviousDungeon.Name = "btnPreviousDungeon"
+        Me.btnPreviousDungeon.Size = New System.Drawing.Size(57, 24)
+        Me.btnPreviousDungeon.TabIndex = 12
+        Me.btnPreviousDungeon.Text = "Previous"
+        Me.btnPreviousDungeon.UseVisualStyleBackColor = True
+        '
+        'btnNextDungeon
+        '
+        Me.btnNextDungeon.Location = New System.Drawing.Point(187, 477)
+        Me.btnNextDungeon.Name = "btnNextDungeon"
+        Me.btnNextDungeon.Size = New System.Drawing.Size(57, 24)
+        Me.btnNextDungeon.TabIndex = 13
+        Me.btnNextDungeon.Text = "Next"
+        Me.btnNextDungeon.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 543)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(158, 24)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Floor Selection:"
+        '
+        'cboxhiddenDungeonSelected
+        '
+        Me.cboxhiddenDungeonSelected.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboxhiddenDungeonSelected.FormattingEnabled = True
+        Me.cboxhiddenDungeonSelected.Location = New System.Drawing.Point(250, 477)
+        Me.cboxhiddenDungeonSelected.Name = "cboxhiddenDungeonSelected"
+        Me.cboxhiddenDungeonSelected.Size = New System.Drawing.Size(17, 21)
+        Me.cboxhiddenDungeonSelected.TabIndex = 15
+        Me.cboxhiddenDungeonSelected.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(441, 558)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(135, 13)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Program made by SirPudge"
+        '
+        'picBoxDungeon
+        '
+        Me.picBoxDungeon.Image = Global.PMD_Blue_S_and_Q_Route_Helper.My.Resources.Resources.Tiny_Woods__1F
+        Me.picBoxDungeon.Location = New System.Drawing.Point(12, 12)
+        Me.picBoxDungeon.Name = "picBoxDungeon"
+        Me.picBoxDungeon.Size = New System.Drawing.Size(560, 436)
+        Me.picBoxDungeon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBoxDungeon.TabIndex = 0
+        Me.picBoxDungeon.TabStop = False
+        '
+        'PMDmainForm
+        '
+        Me.AcceptButton = Me.btnNextFloor
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(584, 580)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.cboxhiddenDungeonSelected)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnNextDungeon)
+        Me.Controls.Add(Me.btnPreviousDungeon)
+        Me.Controls.Add(Me.cboxFloor)
+        Me.Controls.Add(Me.lblManual)
+        Me.Controls.Add(Me.cboxDungeon)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblStaticNotes)
+        Me.Controls.Add(Me.btnNextFloor)
+        Me.Controls.Add(Me.btnPreviousFloor)
+        Me.Controls.Add(Me.lblFloor)
+        Me.Controls.Add(Me.lblStaticFloor)
+        Me.Controls.Add(Me.lblDungeon)
+        Me.Controls.Add(Me.lblStaticDungeon)
+        Me.Controls.Add(Me.picBoxDungeon)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "PMDmainForm"
+        Me.Text = "PMD Blue S and Q Route Helper"
+        CType(Me.picBoxDungeon, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picBoxDungeon As PictureBox
+    Friend WithEvents lblStaticDungeon As Label
+    Friend WithEvents lblDungeon As Label
+    Friend WithEvents lblStaticFloor As Label
+    Friend WithEvents lblFloor As Label
+    Friend WithEvents btnPreviousFloor As Button
+    Friend WithEvents btnNextFloor As Button
+    Friend WithEvents lblStaticNotes As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cboxDungeon As ComboBox
+    Friend WithEvents lblManual As Label
+    Friend WithEvents cboxFloor As ComboBox
+    Friend WithEvents btnPreviousDungeon As Button
+    Friend WithEvents btnNextDungeon As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cboxhiddenDungeonSelected As ComboBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ PMD Blue S and Q Route Helper/PMDmainForm.vb
@@ -0,0 +1,122 @@
+﻿Public Class PMDmainForm
+
+    Private ReadOnly dungeonsWhole() As String = {"Tiny Woods", "Thunderwave Cave", "Mt. Steel", "Sinister Woods", "Silent Chasm", "Mt. Thunder", "Mt. Thunder Peak", "Great Canyon", "Lapis Cave", "Mt. Blaze", "Mt. Blaze Peak", "Frosty Forest", "Frosty Grotto", "Mt. Freeze", "Mt. Freeze Peak", "Uproar Forest", "Magma Cavern", "Magma Cavern Pit", "Sky Tower", "Sky Tower Summit"} 'All Dungeons seeable in game first part of their name
+
+    Private ReadOnly dungeonsPart1() As String = {"Tiny", "Thunderwave", "Mt", "Sinister", "Silent", "Mt", "Mt", "Great", "Lapis", "Mt", "Mt", "Frosty", "Frosty", "Mt", "Mt", "Uproar", "Magma", "Magma", "Sky", "Sky"} 'All Dungeons seeable in game first part of their name
+
+    Private ReadOnly dungeonsPart2() As String = {"Woods", "Cave", "Steel", "Woods", "Chasm", "Thunder", "Thunder", "Canyon", "Cave", "Blaze", "Blaze", "Forest", "Grotto", "Freeze", "Freeze", "Forest", "Cavern", "Cavern", "Tower", "Tower"} 'All Dungeons seeable in game second part of their name
+
+    Private ReadOnly dungeonsPart3() As String = {"", "", "", "", "", "", "Peak", "", "", "", "Peak", "", "", "", "Peak", "", "", "Pit", "", "Summit"} 'All Dungeons seeable in game third part of their name. Only Peaks or dungeons within dungeons need this extra part
+
+    Private ReadOnly maxFloors() As Byte = {3, 5, 9, 12, 9, 10, 3, 12, 14, 12, 3, 9, 5, 15, 4, 9, 23, 1, 25, 9} 'Max floors per dungeon, runs parallel to dungeons()
+
+    Private ReadOnly floors() As Byte = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25} 'All floors seeable in game  ;  The starting zero is a placeholder number so that maxFloors does not have to be -1 to every call (
+
+    Private Const MAX_FLOOR As Byte = 25
+    Private Const MIN_FLOOR As Byte = 1
+    Private Const UNDERSCORE As Char = "_"
+    Private Const CHARACTERF As Char = "F"
+
+    Private dungeonIndexTracker = 0 'To keep track of what dungeon is next or previous - could not figure out a non global way to do this
+    Private floorIndexTracker = 1   'To keep track of what floor is next or previous - could not figure out a non global way to do this
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        loadArrays()
+        cboxDungeon.SelectedIndex = 0
+        cboxhiddenDungeonSelected.SelectedIndex = 0
+        cboxFloor.SelectedIndex = 0
+        'lblDungeon.Text = dungeonsWhole(1)
+        'lblFloor.Text = floors(1).ToString & "F"
+    End Sub
+
+    Private Sub loadArrays() 'sets both comboboxes with appropriate arrays for initial load. Only used once
+        For index = 0 To dungeonsWhole.Length - 1
+            cboxDungeon.Items.Add(dungeonsWhole(index))
+            cboxhiddenDungeonSelected.Items.Add(dungeonsWhole(index))
+        Next
+
+        'For index = 1 To floors.Length - 1
+        'cboxFloor.Items.Add(floors(index))
+        'Next
+
+        For index = MIN_FLOOR To 3
+            cboxFloor.Items.Add(floors(index))
+        Next
+
+    End Sub
+
+    Private Sub btnPreviousDungeon_Click(sender As Object, e As EventArgs) Handles btnPreviousDungeon.Click
+        If cboxhiddenDungeonSelected.SelectedIndex > 0 Then
+            cboxhiddenDungeonSelected.SelectedIndex = cboxhiddenDungeonSelected.SelectedIndex - 1
+            lblDungeon.Text = dungeonsWhole(cboxhiddenDungeonSelected.SelectedIndex)
+            setMaxFloors()
+            resetFloorLabel()
+            dungeonHasChanged()
+            pictureHasChanged()
+        End If
+    End Sub
+
+    Private Sub btnNextDungeon_Click(sender As Object, e As EventArgs) Handles btnNextDungeon.Click
+        If cboxhiddenDungeonSelected.SelectedIndex < dungeonsWhole.Length - 1 Then
+            cboxhiddenDungeonSelected.SelectedIndex = cboxhiddenDungeonSelected.SelectedIndex + 1
+            lblDungeon.Text = dungeonsWhole(cboxhiddenDungeonSelected.SelectedIndex)
+            setMaxFloors()
+            resetFloorLabel()
+            dungeonHasChanged()
+            pictureHasChanged()
+        End If
+    End Sub
+
+    Private Sub btnPreviousFloor_Click(sender As Object, e As EventArgs) Handles btnPreviousFloor.Click
+        If floors(floorIndexTracker) > MIN_FLOOR Then
+            floorIndexTracker = floorIndexTracker - MIN_FLOOR
+            lblFloor.Text = floors(floorIndexTracker) & CHARACTERF
+            pictureHasChanged()
+        End If
+    End Sub
+
+    Private Sub btnNextFloor_Click(sender As Object, e As EventArgs) Handles btnNextFloor.Click
+        If floors(floorIndexTracker) < maxFloors(cboxhiddenDungeonSelected.SelectedIndex) Then
+            floorIndexTracker = floorIndexTracker + MIN_FLOOR
+            lblFloor.Text = floors(floorIndexTracker) & CHARACTERF
+            pictureHasChanged()
+        End If
+    End Sub
+
+    Private Sub setMaxFloors()
+        cboxFloor.Items.Clear()
+
+        For index = MIN_FLOOR To maxFloors(cboxhiddenDungeonSelected.SelectedIndex)
+            cboxFloor.Items.Add(floors(index))
+        Next
+    End Sub
+
+    Private Sub resetFloorLabel()
+        floorIndexTracker = MIN_FLOOR
+        lblFloor.Text = floors(floorIndexTracker) & CHARACTERF
+    End Sub
+
+    Private Sub dungeonHasChanged()
+        cboxDungeon.SelectedIndex = cboxhiddenDungeonSelected.SelectedIndex
+        cboxFloor.SelectedIndex = 0
+    End Sub
+
+    Private Sub pictureHasChanged() 'Handles changing the picture after any update
+        picBoxDungeon.Image = My.Resources.ResourceManager.GetObject((dungeonsPart1(cboxhiddenDungeonSelected.SelectedIndex) & UNDERSCORE & dungeonsPart2(cboxhiddenDungeonSelected.SelectedIndex) & UNDERSCORE & dungeonsPart3(cboxhiddenDungeonSelected.SelectedIndex) & UNDERSCORE & floors(floorIndexTracker) & CHARACTERF))
+    End Sub
+
+    Private Sub cboxDungeon_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboxDungeon.SelectedIndexChanged
+        cboxhiddenDungeonSelected.SelectedIndex = cboxDungeon.SelectedIndex
+        lblDungeon.Text = dungeonsWhole(cboxhiddenDungeonSelected.SelectedIndex)
+        setMaxFloors()
+        resetFloorLabel()
+        dungeonHasChanged()
+        pictureHasChanged()
+    End Sub
+
+    Private Sub cboxFloor_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboxFloor.SelectedIndexChanged
+        floorIndexTracker = cboxFloor.SelectedIndex + MIN_FLOOR
+        lblFloor.Text = floors(floorIndexTracker) & CHARACTERF
+        'cboxFloor.SelectedIndex = 0
+    End Sub
+End Class
